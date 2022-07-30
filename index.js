1c1db5dbcd769214f95b560ad88084b4853ff6be
Maide Yıldız <maideyildizz@gmail.com>
2022-07-30 19:01:07 +0300

    added server.js

--- index.js
+++ index.js
@@ -8,7 +8,4 @@ app.use(morgan("dev"));
 // app.use('/api/v1/blog', blogRouter);
 // app.use('/api/v1/users', userRouter);
 
-const port = 3000;
-app.listen(port, () => {
-  console.log(`App running on port ${port}...`);
-});
+module.exports = app;
